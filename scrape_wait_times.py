c30044097358d6984935fa62d89a38e00bdf8b02
Rodrigo Amaro <rodriamarog@gmail.com>
2024-03-14 19:27:50 -0700

    Added Otay times

--- scrape_wait_times.py
+++ scrape_wait_times.py
@@ -15,7 +15,7 @@ def scrape_wait_times():
         driver.get("https://www.smartbordercoalition.com/border-wait-times")
         driver.implicitly_wait(10)
 
-        wait_times = driver.execute_script("""
+        sy_wait_times = driver.execute_script("""
             let times = [];
             let tickerItems = document.querySelectorAll("div.ticker__item:nth-of-type(2) span");
             tickerItems.forEach(function(item) {
@@ -24,11 +24,20 @@ def scrape_wait_times():
             return times;
         """)
 
+        otay_wait_times = driver.execute_script("""
+            let times = [];
+            let tickerItems = document.querySelectorAll("div.ticker__item:nth-of-type(3) span");
+            tickerItems.forEach(function(item) {
+                times.push(item.innerText);
+            });
+            return times;
+        """)
+
         lanes = ['All Traffic >>', 'Ready Lanes >>', 'Sentri >>']
         count = 0
-        filtered_wait_times = ['Normal Lanes >>']
+        filtered_wait_times = ['SAN YSIDRO:','All Traffic >>']
 
-        for wait_time in wait_times:
+        for wait_time in sy_wait_times:
             if wait_time[-1] == ":" or wait_time[0] == "N":
                 continue
             else:
@@ -44,9 +53,36 @@ def scrape_wait_times():
                     count += 1
                     filtered_wait_times.append("\n" + lanes[count])
 
-        print(filtered_wait_times)
-        return filtered_wait_times
+        lanes_otay = ['All Traffic >>', 'Ready Lanes >>', 'Sentri >>']
+        count_otay = 0
+        filtered_wait_times_otay = ['\n-\n-\nOTAY:','All Traffic >>']
 
+        for wait_time in otay_wait_times:
+            if wait_time[-1] == ":" or wait_time[0] == "N":
+                continue
+            else:
+                if 'No Delay' in wait_time:
+                    filtered_wait_times_otay.append(wait_time)                 
+                elif 'Status' in wait_time:
+                    wait_time = 'Vehicles: 0.05'
+                    filtered_wait_times_otay.append(wait_time)                   
+                    continue
+                else:
+                    filtered_wait_times_otay.append(wait_time)                  
+                if 'Pedestrians' in wait_time:
+                    count_otay += 1
+                    filtered_wait_times_otay.append("\n" + lanes_otay[count_otay])
+
+        filtered_wait_times_otay = filtered_wait_times_otay[:-2]
+
+
+        wait_times_combined = filtered_wait_times + filtered_wait_times_otay
+
+        for i in wait_times_combined:
+            print(i)
+
+        return wait_times_combined
+    
     except Exception as e:
         print(f'An error occurred: {e}')
     
