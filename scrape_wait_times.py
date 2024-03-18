b9a18b03a5b88ae2e9ebc206bdf3dd830a1c433f
Rodrigo Amaro <rodriamarog@gmail.com>
2024-03-18 15:57:01 -0700

    code refactoring. function for scrape_wait_times

--- scrape_wait_times.py
+++ scrape_wait_times.py
@@ -1,5 +1,33 @@
 from selenium import webdriver
 
+lanes_sy = ['All Traffic >>', 'Ready Lanes >>', 'Sentri >>']
+count_sy = 0
+filtered_wait_times_sy = ['SAN YSIDRO:','All Traffic >>']
+
+lanes_otay = ['All Traffic >>', 'Ready Lanes >>', 'Sentri >>']
+count_otay = 0
+filtered_wait_times_otay = ['\n-\n-\nOTAY:','All Traffic >>']
+
+def process_wait_times(wait_times, lanes):
+    count = 0
+    filtered_wait_times = ['All Traffic >>']
+    for wait_time in wait_times:
+        if wait_time[-1] == ":" or wait_time[0] == "N":
+            continue
+        else:
+            if 'No Delay' in wait_time:
+                filtered_wait_times.append(wait_time)                 
+            elif 'Status' in wait_time:
+                wait_time = 'Vehicles: 0.05'
+                filtered_wait_times.append(wait_time)                   
+                continue
+            else:
+                filtered_wait_times.append(wait_time)                  
+            if 'Pedestrians' in wait_time:
+                count += 1
+                filtered_wait_times.append("\n" + lanes[count])
+    return filtered_wait_times
+
 def scrape_wait_times():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
@@ -33,61 +61,22 @@ def scrape_wait_times():
             return times;
         """)
 
-        lanes = ['All Traffic >>', 'Ready Lanes >>', 'Sentri >>']
-        count = 0
-        filtered_wait_times = ['SAN YSIDRO:','All Traffic >>']
+        filtered_wait_times_otay = process_wait_times(otay_wait_times, lanes_otay)
+        filtered_wait_times_otay.insert(0, '\n-\n-\nOTAY:')
+        filtered_wait_times_sy = process_wait_times(sy_wait_times, lanes_sy)
+        filtered_wait_times_sy.insert(0, 'SAN YSIDRO:')
 
-        for wait_time in sy_wait_times:
-            if wait_time[-1] == ":" or wait_time[0] == "N":
-                continue
-            else:
-                if 'No Delay' in wait_time:
-                    filtered_wait_times.append(wait_time)                 
-                elif 'Status' in wait_time:
-                    wait_time = 'Vehicles: 0.05'
-                    filtered_wait_times.append(wait_time)                   
-                    continue
-                else:
-                    filtered_wait_times.append(wait_time)                  
-                if 'Pedestrians' in wait_time:
-                    count += 1
-                    filtered_wait_times.append("\n" + lanes[count])
+        combined_wait_times = [filtered_wait_times_sy,filtered_wait_times_otay]
 
-        lanes_otay = ['All Traffic >>', 'Ready Lanes >>', 'Sentri >>']
-        count_otay = 0
-        filtered_wait_times_otay = ['\n-\n-\nOTAY:','All Traffic >>']
-
-        for wait_time in otay_wait_times:
-            if wait_time[-1] == ":" or wait_time[0] == "N":
-                continue
-            else:
-                if 'No Delay' in wait_time:
-                    filtered_wait_times_otay.append(wait_time)                 
-                elif 'Status' in wait_time:
-                    wait_time = 'Vehicles: 0.05'
-                    filtered_wait_times_otay.append(wait_time)                   
-                    continue
-                else:
-                    filtered_wait_times_otay.append(wait_time)                  
-                if 'Pedestrians' in wait_time:
-                    count_otay += 1
-                    filtered_wait_times_otay.append("\n" + lanes_otay[count_otay])
-
-        filtered_wait_times_otay = filtered_wait_times_otay[:-2]
-
-
-        wait_times_combined = filtered_wait_times + filtered_wait_times_otay
-
-        for i in wait_times_combined:
-            print(i)
-
-        return wait_times_combined
+        return combined_wait_times
     
     except Exception as e:
         print(f'An error occurred: {e}')
     
     finally:
         driver.quit()
+
+print(scrape_wait_times())
 
 if __name__ == '__main__':
     scrape_wait_times()
